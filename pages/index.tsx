c3f79d5b823909509b43af1d4307dda8ce5451f3
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-07 22:31:52 +0700

    getData

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
-const Index = () => {
+import { GetServerSideProps, InferGetStaticPropsType } from "next"
+
+interface KData {
+	id: Int16Array
+}
+
+const Index = ({ data }) => {
 	return (
 		<div>
-			<h1>Hello World</h1>
-			<p>Running on Preact X with Next.js!</p>
+			{data.map((r) => (
+				<li>{r.id}</li>
+			))}
 		</div>
 	)
 }
 
+export const getServerSideProps: GetServerSideProps = async () => {
+	const res = await fetch("https://konachan.com/post.json?page=1")
+	const data = await res.json()
+	return {
+		props: { data },
+	}
+}
 export default Index
