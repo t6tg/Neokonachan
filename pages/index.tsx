50128ca2f47c3bac7f32ef5c127da83aa87bad78
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-07 23:34:36 +0700

    update state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
-import { GetServerSideProps, InferGetStaticPropsType } from "next"
-
-interface KData {
-	id: Int16Array
-}
+import { GetServerSideProps } from "next"
+import { useState } from "react"
 
 const Index = ({ data }) => {
+	const [loading, setLoading] = useState(true)
 	return (
-		<div>
+		<div className="grid grid-flow-rows grid-cols-4 gap-2 mx-auto">
 			{data.map((r) => (
-				<li>{r.id}</li>
+				<div>
+					<img
+						className="rounded-lg"
+						src={r.preview_url}
+						alt={r.id}
+						width="300px"
+					/>
+				</div>
 			))}
 		</div>
 	)
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
+	// mockup data
 	const res = await fetch("https://konachan.com/post.json?page=1")
 	const data = await res.json()
 	return {
